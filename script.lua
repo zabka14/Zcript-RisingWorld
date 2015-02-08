20fa6afd0d7fbd9d08c39e73485b39eabed0c4bb
zabka14 <vianey.benjamin@gmail.com>
2015-02-08 06:42:54 +0100

    voilà voilà !

--- script.lua
+++ script.lua
@@ -29,12 +29,20 @@ yellLabel:setBorderThickness(4);
 yellLabel:setFontsize(30);
 yellLabel:setPivot(4);
 
+
+
+            ---------------------------------------------------------------------
+            --                         Event Handler                           --
+            ---------------------------------------------------------------------
+
+
 function onPlayerSpawn(event)
     event.player:addGuiElement(yellLabel)
     broadcastPlayerStatus(event.player, " joined the world")
     showWelcome(event.player);
     -- check for players that were offline when banned
     checkban(event.player)
+    event.player:sendTextMessage("[#00FFCC]This server is using Zcript ");
 end
 
 function onPlayerConnect(event)
@@ -67,28 +75,68 @@ function onPlayerCommand(event)
     if string.sub(event.command,1,1) == "/" then
         local cmd = explode(" ", event.command, 2)
         cmd[1] = string.lower(cmd[1])
-    
+
+            ---------------------------------------------------------------------
+            --                         Help command                            --
+            ---------------------------------------------------------------------
+            
         if cmd[1] == "/help" then
+                event.player:sendTextMessage("[#00FFCC]/ahelp [#00CC88] Display admin help");
+                event.player:sendTextMessage("[#00FFCC]/zhelp [#00CC88] Display common help");
+               -- event.player:sendTextMessage("[#00FFCC]/ghelp [#00CC88] Display group management help");
+
+        elseif cmd[1] == "/ahelp" then
             if event.player:isAdmin() then
                 event.player:sendTextMessage("[#00FFCC]/ban [#00CC88]<player> <duration in minutes, -1 is permenant> <reason>");
                 event.player:sendTextMessage("[#00FFCC]/unban [#00CC88]<player>");
                 event.player:sendTextMessage("[#00FFCC]/setWelcome [#00CC88]<message>");
                 event.player:sendTextMessage("[#00FFCC]/setMotd [#00CC88]<message>");
                 event.player:sendTextMessage("[#00FFCC]/yell [#00CC88]<message>");
-                -- Zcript added functions :
                 event.player:sendTextMessage("[#00FFCC]/kill [#00CC88] <ID>");
                 event.player:sendTextMessage("[#00FFCC]/kill2 [#00CC88]<player>");
-                event.player:sendTextMessage("[#00FFCC]/tp [#00CC88] <ID OR player name>"); -- This function teleport an admin to a player
-                event.player:sendTextMessage("[#00FFCC]/tp2 [#00CC88] <ID OR player name>"); -- This function teleport a player to an admin
-                event.player:sendTextMessage("[#00FFCC]/kick <player ID> <reason>");
+                event.player:sendTextMessage("[#00FFCC]/kick [#00CC88] <player ID> <reason>");
+            else
+                event.player:sendTextMessage("[#00FFCC]You're not an admin !");
             end
+
+       -- elseif cmd[1] == "/ghelp" then
+       --         event.player:sendTextMessage("[#00FFCC]/gkick [#00CC88] <playerID> OR <player Name> ");
+       --         event.player:sendTextMessage("[#00FFCC]/gadd [#00CC88] <playerID> OR <player Name> ");
+       --         event.player:sendTextMessage("[#00FFCC]/g [#00CC88] <message> ");
+       --         event.player:sendTextMessage("[#00FFCC]/gsetAdmin [#00CC88] <playerID> OR <player Name> ");
+
+
+        elseif cmd[1] == "/zhelp" then
             event.player:sendTextMessage("[#00FFCC]/last [#00CC88][player]");
             event.player:sendTextMessage("[#00FFCC]/whisper [#00CC88]<player> <message>");
-            -- Zcript added function
             event.player:sendTextMessage("[#00FFCC]/pos");
+            event.player:sendTextMessage("[#00FFCC]/tp [#00CC88] <ID> OR <player name>");
+            event.player:sendTextMessage("[#00FFCC]/tp2 [#00CC88] <ID> OR <player name>");
 
 
 
+            ---------------------------------------------------------------------
+            --             Calling command based on console entry              --
+            ---------------------------------------------------------------------
+            
+            ----------------------------
+            --     Group Management   --
+            ----------------------------
+   --     elseif cmd[1] == "/gkick" then
+
+
+    --    elseif cmd[1] == "/gadd" then
+
+
+  --      elseif cmd[1] == "/g" then
+
+
+  --      elseif cmd[1] == "/gsetAdmin" then    
+
+
+            ----------------------------
+            --       For Admin        --
+            ----------------------------
 
         elseif cmd[1] == "/kick" then
             -- Checking if admin :
@@ -118,9 +166,8 @@ function onPlayerCommand(event)
             if not server:findPlayerByID(cmd[2]) then return msgBadID(event.player) end
             local target = server:findPlayerByID(cmd[2]);
             kill(event.player, target)
-        
-        -- TP admin -> player 
 
+        -- TP admin -> player 
         elseif cmd[1] == "/tp" then
             -- Checking if admin :
             if not event.player:isAdmin() then return msgAccessDenied(event.player) end
@@ -136,11 +183,11 @@ function onPlayerCommand(event)
                 -- Checking if targeted player exist
                 if not server:findPlayerByName(cmd[2]) then return msgBadID(event.player) end
                 local target = server:findPlayerByName(cmd[2]);
-            -- Checking if targeted player exist
+            end
             tp(event.player, target)
         
 
-        -- TP player -> player (or admin)
+        -- TP player -> admin
         elseif cmd[1] == "/tp2" then
             -- Checking if admin :
             if not event.player:isAdmin() then return msgAccessDenied(event.player) end
@@ -156,13 +203,10 @@ function onPlayerCommand(event)
                 -- Checking if targeted player exist
                 if not server:findPlayerByName(cmd[2]) then return msgBadID(event.player) end
                 local target = server:findPlayerByName(cmd[2]);
-            -- Checking if targeted player exist
+            end
             tp(target, event.player)
 
 
-        elseif cmd[1] == "/pos" then
-            local pos = event.player:getPlayerPosition();
-            event.player:sendTextMessage(pos)
 
         elseif cmd[1] == "/ban" then
             if not event.player:isAdmin() then return msgAccessDenied(event.player) end
@@ -170,20 +214,31 @@ function onPlayerCommand(event)
             local args = explode(" ", cmd[2], 3)
         if not args[1] or not args[2] or not args[3] then return msgInvalidUsage(event.player) end
         ban(args[1], args[2], args[3], event.player)
+
          elseif cmd[1] == "/unban" then
             if not event.player:isAdmin() then return msgAccessDenied(event.player) end
             if not cmd[2] then return msgInvalidUsage(event.player) end
             unban(cmd[2], event.player)
+
         elseif cmd[1] == "/setmotd" then
             if not event.player:isAdmin() then return msgAccessDenied(event.player) end
             if not cmd[2] then return msgInvalidUsage(event.player) end
             setMotd(cmd[2])
             event.player:sendTextMessage("[#00FFCC]motd set");
+
         elseif cmd[1] == "/setwelcome" then
             if not event.player:isAdmin() then return msgAccessDenied(event.player) end
             if not cmd[2] then return msgInvalidUsage(event.player) end
             setWelcome(cmd[2])
             event.player:sendTextMessage("[#00FFCC]welcome set");
+
+
+
+            ----------------------------
+            --       For all          --
+            ----------------------------
+
+
         elseif cmd[1] == "/yell" then
             if not event.player:isAdmin() then return msgAccessDenied(event.player) end
             if not cmd[2] then return msgInvalidUsage(event.player) end
@@ -195,8 +250,16 @@ function onPlayerCommand(event)
             setTimer(function()
                     yellLabel:setVisible(false);
             end, 5, 1);
+
         elseif cmd[1] == "/last" then
         sendTableMessage{player=event.player, messages=getLastText{name=cmd[2]}}
+
+
+        elseif cmd[1] == "/pos" then
+            local pos = event.player:getPlayerPosition();
+            event.player:sendTextMessage(pos)
+
+
         elseif cmd[1] == "/whisper" then
             if not cmd[2] then return msgInvalidUsage(event.player) end
             local args = explode(" ", cmd[2], 2)
@@ -210,6 +273,12 @@ function onPlayerCommand(event)
     end
 end
 
+
+
+            ---------------------------------------------------------------------
+            --           Effective command called by previous part             --
+            ---------------------------------------------------------------------
+            
 
 
 function kickPlayer(kicker, target, reason)
@@ -237,13 +306,25 @@ end
 
 function tp(admin, player)
     local targetPos = player:getPlayerPosition();
-    local newPosx = targetPos.x + 10.0;
-    local newPosy = targetPos.y + 10.0;
-    local newPosz = targetPos.z + 10.0;
+    local newPosx = targetPos.x + 1.0;
+    local newPosy = targetPos.y + 1.0;
+    local newPosz = targetPos.z + 1.0;
     -- it seems that admin:setPlayerPosition(targetPos.x +1.0, targetPos.y +1.0, targetPos.z + 1.0) doesn't work ...
-    -- so I try to add +10 to x y and z before calling the function
+    -- so I try to add +1.0 to x y and z before calling the function
     admin:setPlayerPosition(newPosx, newPosy, newPosz); 
 end
+
+function tp(admin, player)
+    local targetPos = player:getPlayerPosition();
+    local newPosx = targetPos.x + 1.0;
+    local newPosy = targetPos.y + 1.0;
+    local newPosz = targetPos.z + 1.0;
+    -- it seems that admin:setPlayerPosition(targetPos.x +1.0, targetPos.y +1.0, targetPos.z + 1.0) doesn't work ...
+    -- so I try to add +1.0 to x y and z before calling the function
+    admin:setPlayerPosition(newPosx, newPosy, newPosz); 
+end
+
+
 
 function decoratePlayerName(player)
     local str = "[#CCCCCC]"
@@ -412,12 +493,27 @@ function lastlog(opts)
     database:queryupdate(query)
 end
 
+
+
+
+            ---------------------------------------------------------------------
+            --                         Adding events                           --
+            ---------------------------------------------------------------------
+            
+
+
 addEvent("PlayerSpawn", onPlayerSpawn);
 addEvent("PlayerConnect", onPlayerConnect);
 addEvent("PlayerDisconnect", onPlayerDisconnect);
 addEvent("PlayerDeath", onPlayerDeath);
 addEvent("PlayerText", onPlayerText);
 addEvent("PlayerCommand", onPlayerCommand);
+
+
+            ---------------------------------------------------------------------
+            --              On enable function, called when server start       --
+            ---------------------------------------------------------------------
+
 
 function onEnable()
     print(timePrefix{text="Loaded"});
@@ -426,6 +522,13 @@ function onEnable()
     database:queryupdate("CREATE TABLE IF NOT EXISTS `settings` (`key` PRIMARY KEY NOT NULL, `value` VARCHAR);");
     database:queryupdate("CREATE TABLE IF NOT EXISTS `banlist` (`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `playername` NOT NULL, `admin` VARCHAR, `serial` VARCHAR, `date` INTEGER NOT NULL, `duration` LONG DEFAULT -1, `reason` VARCHAR, `applied_at` BOOLEAN DEFAULT 0);");
     database:queryupdate("CREATE TABLE IF NOT EXISTS `lastlog` (`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `player_id` INTEGER, `name` VARCHAR, `ip` VARCHAR, `connect_at` INTEGER, `disconnect_at` INTEGER DEFAULT -1)");
+
+
+    -- Group management DB entry
+    database:queryupdate("CREATE TABLE IF NOT EXISTS `player` (`ID` INTEGER PRIMARY KEY NOT NULL, `name` VARCHAR NOT NULL, `groupID` INTEGER);");
+    database:queryupdate("CREATE TABLE IF NOT EXISTS `group` (`ID` INTEGER PRIMARY KEY NOT NULL, `name` VARCHAR NOT NULL, `adminID` INTEGER NOT NULL);");
+    database:queryupdate("CREATE TABLE IF NOT EXISTS `groupAdmin` (`ID` INTEGER PRIMARY KEY NOT NULL, `admin1` INTEGER, `admin2` INTEGER, `admin3` INTEGER);");
+
 
     -- Cleanup lost connections (server crash)
     database:queryupdate("UPDATE `lastlog` SET `disconnect_at` = 0 WHERE `disconnect_at` = -1")
